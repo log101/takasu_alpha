c5ef039351ee324e0e3c3b681486608ded4e3e26
log101 <frknerdm@mail.com>
2022-04-10 09:53:16 +0300

    add seed data and photos

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,35 +13,90 @@ User.create({
               description: "Kitap okumayı ve müzik dinlemeyi severim, yazılımla uğraşıyorum. Üniversitede öğrenciyim, üsküdar sahile çok yakın bir yerde oturuyorum. En sevdiğim anime Cowboy Bebop.",
               password_digest: BCrypt::Password.create("furkan")
             })
+User.create({
+              id: 2,
+              name: "erdem",
+              email: "erdem",
+              description: "Hayatı boyun uzaya çıkmak istemişimdir. Bu dünyada sorduğum soruların cevapları belki orada yukarıdadır. Onun dışında manga okumayı seviyorum, özellikle spor magalarını!",
+              password_digest: BCrypt::Password.create("erdem"),
+            })
 Manga.create({
-               title: "Manga 1",
-               genre: "Genre 1",
-               description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-               volumes: "1-13, 15, 17",
+               title: "Akatsuki no Yona",
+               genre: "Action",
+               description: "Yona's perfect world comes crashing down when a heinous act of treason threatens to erase all that she holds dear, including her birthright as the princess of Kouka. Left with no one to trust but her childhood friend and loyal bodyguard Son Hak, she is forced to flee the palace. Faced with the perils of surviving in the wild with a target on her back, Yona realizes that her kingdom is no longer the safe haven it once was.",
+               volumes: "1-13",
                user_id: 1,
-               image_url: "alchemist.jpg"
+               image_url: "akatsuki-no-yona.jpg"
              })
 Manga.create({
-               title: "Manga 2",
-               genre: "Genre 2",
-               description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-               volumes: "1-13, 15, 17",
-               user_id: 1,
-               image_url: "alchemist.jpg"
+               title: "Berserk",
+               genre: "Gore",
+               description: "Guts, a former mercenary now known as the Black Swordsman, is out for revenge. After a tumultuous childhood, he finally finds someone he respects and believes he can trust, only to have everything fall apart when this person takes away everything important to Guts for the purpose of fulfilling his own desires. Now marked for death, Guts becomes condemned to a fate in which he is relentlessly pursued by demonic beings.",
+               volumes: "1-4, 6-9",
+               user_id: 2,
+               image_url: "berserk.jpg",
              })
 Manga.create({
-               title: "Manga 3",
-               genre: "Genre 3",
-               description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-               volumes: "1-13, 15, 17",
+               title: "Ansatsu Kyoushitsu",
+               genre: "Action",
+               description: "Adapted from the humorous hit manga by Yuusei Matsui, Ansatsu Kyoushitsu tells the story of these junior high pupils as they polish their assassination skills and grow in order to stand strong against the oppressive school system, their own life problems, and one day, Koro-sensei.",
+               volumes: "1-21",
                user_id: 1,
-               image_url: "alchemist.jpg"
+               image_url: "ansatsu.jpg",
              })
 Manga.create({
-               title: "Manga 4",
-               genre: "Genre 4",
-               description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-               volumes: "1-13, 15, 17",
+               title: "GTO",
+               genre: "Action",
+               description: "Great Teacher Onizuka follows the incredible, though often ridiculous, antics of the titular teacher as he attempts to outwit and win over the cunning Class 3-4 that is determined to have him removed from the school. However, other obstacles present themselves throughout—including the frustrated, balding vice principal, Hiroshi Uchiyamada; old enemies from his biker days; and his own idiotic teaching methods. But Eikichi fights it all whilst trying to help his students, romance fellow teacher Azusa Fuyutsuki, and earn his self-proclaimed title.",
+               volumes: "1-2",
+               user_id: 2,
+               image_url: "gto.jpg",
+             })
+Manga.create({
+               title: "Haikyuu!",
+               genre: "Sports",
+               description: "Great Teacher Onizuka follows the incredible, though often ridiculous, antics of the titular teacher as he attempts to outwit and win over the cunning Class 3-4 that is determined to have him removed from the school. However, other obstacles present themselves throughout—including the frustrated, balding vice principal, Hiroshi Uchiyamada; old enemies from his biker days; and his own idiotic teaching methods. But Eikichi fights it all whilst trying to help his students, romance fellow teacher Azusa Fuyutsuki, and earn his self-proclaimed title.",
+               volumes: "1-2",
                user_id: 1,
-               image_url: "alchemist.jpg"
+               image_url: "haikyuu.jpg",
+             })
+Manga.create({
+               title: "Kimetsu no Yaiba",
+               genre: "Fantasy",
+               description: "When he finally makes his way home, Tanjirou's worst nightmare comes true. His entire family has been brutally slaughtered with the sole exception of his sister Nezuko, who has turned into a flesh-eating demon. Engulfed in hatred and despair, Tanjirou desperately tries to stop Nezuko from attacking other people, setting out on a journey to avenge his family and find a way to turn his beloved sister back into a human.",
+               volumes: "1-3",
+               user_id: 2,
+               image_url: "kimetsu.jpg",
+             })
+Manga.create({
+               title: "Made In Abyss",
+               genre: "Action",
+               description: "One day, Riko's life changes when she meets a strange robot called Reg, who seems to appear from within the Abyss. Believing this to be a sign from her mother stuck at the bottom of the Abyss, Riko descends into its depths with Reg, ready to confront all the dangers within it.",
+               volumes: "1-2, 6-9",
+               user_id: 1,
+               image_url: "made-in-abyss.jpg",
+             })
+Manga.create({
+               title: "Monster",
+               genre: "Drama",
+               description: "Kenzou Tenma, a renowned Japanese neurosurgeon working in post-war Germany, faces a difficult choice: to operate on Johan Liebert, an orphan boy on the verge of death, or on the mayor of Düsseldorf. In the end, Tenma decides to gamble his reputation by saving Johan, effectively leaving the mayor for dead.",
+               volumes: "1-3",
+               user_id: 2,
+               image_url: "monster.jpg",
+             })
+Manga.create({
+               title: "Kingdom",
+               genre: "Action, Drama",
+               description: "Although initially on bad terms, Xin and Zheng become comrades and start on a path filled with trials and bloodshed. Zheng's objective is to bring all the warring states under Qin, and Xin seeks to climb to the very top of the army ranks. Against a backdrop of constant tactical battle between states and great political unrest, both outside and within the palace, the two endeavor towards their monumental ambitions that will change history forever.",
+               volumes: "1-14",
+               user_id: 1,
+               image_url: "kingdom.jpg",
+             })
+Manga.create({
+               title: "Full Metal Alchemist",
+               genre: "Action",
+               description: "Years later, the young alchemists travel across the country looking for the Philosopher's Stone, in the hopes of recovering their old bodies with its power. However, their quest for the fated stone also leads them to unravel far darker secrets than they could ever imagine.",
+               volumes: "24-27",
+               user_id: 2,
+               image_url: "alchemist.jpg",
              })
